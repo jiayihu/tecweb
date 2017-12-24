55f2b7a451644139c16ea03d8a308ca828404c9b
Jiayi Hu <jiayi.ghu@gmail.com>
2017-12-24 14:09:16 +0100

    servizi: aggiunta bozza pagina

--- source/app/views/servizi.view.php
+++ source/app/views/servizi.view.php
@@ -1,8 +1,97 @@
 <?php require 'partials/public-header.partial.php' ?>
 
 <main class="container">
-  <section class="history">
-    <h1>Servizi (Work in Progress)</h1>
+  <section>
+    <h1>I nostri servizi</h1>
+  </section>
+
+  <section class="service clearfix">
+    <div class="service-content col two-third">
+      <h2>Investigazioni private</h2>
+      <p>
+        Le investigazioni per privati e famiglie mirano a risolvere in maniera personalizzata ogni questione che rientri nella sfera coniugale, familiare, civile e penale.
+        I fattori chiave della propria attività investigativa sono la qualità, la precisione e la velocità.
+      </p>
+      <p>A dispetto degli incredibili progressi tecnologici, è soprattutto grazie all’ottima organizzazione e competenza del proprio team di investigatori privati se oggi è possibile offrire quel valore aggiunto che per noi fa la differenza.</p>
+      <p>I nostri principali servizi di investigazione dedicati ai privati sono:</p>
+      <ul>
+        <li>Infedeltà coniugali</li>
+        <li>Prove per cause di separazione</li>
+        <li>Determinazione assegno familiare</li>
+        <li>Affidamento dei minori</li>
+        <li>Controllo tutela minori</li>
+        <li>Ricerca persone scomparse</li>
+        <li>Accertamenti patrimoniali</li>
+        <li>Investigazioni anti stalking</li>
+      </ul>
+    </div>
+    <div class="service-photo col one-third">
+      <img src="public/images/home/private.jpg"> <!-- alt non importante -->
+    </div>
+  </section>
+
+  <section class="service clearfix">
+    <div class="service-photo col one-third">
+      <img src="public/images/home/agency.jpg"> <!-- alt non importante -->
+    </div>
+    <div class="service-content col two-third">
+    <h2>Investigazioni aziendali</h2>
+    <p>
+      Le investigazioni rivolte ad aziende hanno l’obiettivo di tutelarle da rischi sia interni che esterni. 
+    </p>
+    <p>Abbiamo affrontato in questi anni numerosi casi di illeciti subiti dalle aziende quali cause di assenteismo da parte dei dipendenti, cause di infedeltà da parte dei soci, amministratori e dipendenti, cause di recupero credito.</p>
+    <p>I nostri principali servizi di investigazione dedicati alle aziende sono:</p>
+    <ul>
+      <li>Assenteismo posto di lavoro</li>
+      <li>Concorrenza sleale</li>
+      <li>Tutela del marchio</li>
+      <li>Valutazioni solvibilitá</li>
+      <li>Accompagnamento e logistica</li>
+      <li>Gestione sicurezza</li>
+      <li>Recupero crediti</li>
+    </ul>
+    </div>
+  </section>
+
+  <section class="service clearfix">
+    <div class="service-content col two-third">
+    <h2>Investigazioni legali</h2>
+    <p>
+      La nostra agenzia investigativa è specializzata in investigazioni a supporto degli studi legali sia nell’ambito civile sia nella difesa penale.
+    </p>
+    <p>Le indagini investigative attuate da Vega Investigazioni rappresentano uno strumento rapido ed efficace che si rivela di fondamentale importanza qualora ci sia la necessità di raccogliere informazioni o elementi di prova da far valere nel processo civile o penale in ogni ordine di grado o giudizio.</p>
+    <p>I nostri principali servizi di investigazione legali sono:</p>
+    <ul>
+      <li>Accertamenti patrimoniali</li>
+      <li>Ricerca eredi</li>
+      <li>Indagini difensive</li>
+      <li>Indagini aziendali</li>
+    </ul>
+    </div>
+    <div class="service-photo col one-third">
+      <img src="public/images/home/legal.jpg"> <!-- alt non importante -->
+    </div>
+  </section>
+
+  <section class="service clearfix">
+    <div class="service-photo col one-third">
+      <img src="public/images/home/special.jpg"> <!-- alt non importante -->
+    </div>
+    <div class="service-content col two-third">
+    <h2>Investigazioni cyber-security</h2>
+    <p>
+      In questo particolare periodo storico, dove tutti i device sono connessi tra di loro e la facilità di essere “controllati” è altissima, un servizio di controspionaggio è alla base della sicurezza.
+    </p>
+    <p>Il nostro team di Controspionaggio Informatico si occupa di verificare se nella struttura informatica del cliente vi siano strumenti esterni in grado di monitorare le attività aziendali e produttive, tali da compromettere la stabilità dell’intera azienda.</p>
+    <p>I nostri principali servizi di investigazione dedicati alla cyber-security sono:</p>
+    <ul>
+      <li>Penetration testing</li>
+      <li>Controspionaggio informatico</li>
+      <li>Vulnerability assestment</li>
+      <li>Furto d'identità e truffe online</li>
+      <li>Social Engeneering</li>
+    </ul>
+    </div>
   </section>
 </main>
 
