a0fe2d30c9413b6f30c6bf3f35b2e07ccbb76dc7
Jiayi Hu <jiayi.ghu@gmail.com>
2017-12-27 22:33:26 +0100

    Design: aggiunta favicon

--- source/app/views/partials/admin-header.partial.php
+++ source/app/views/partials/admin-header.partial.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="icon" href="favicon.ico?v=3" type="image/x-icon">
   <title>Studio Sherlock & Watson</title>
 
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
