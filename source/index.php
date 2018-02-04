477b5ccef97443a794ab1f619ee6b3428c36c3a2
Jiayi Hu <jiayi.ghu@gmail.com>
2018-02-04 16:14:53 +0100

    Rinominato bootstrap in main.php

--- source/index.php
+++ source/index.php
@@ -3,7 +3,7 @@
 use Core\Router;
 use Core\Request;
 
-require_once 'core/bootstrap.php';
+require_once 'core/main.php';
 
 Router::load('app/routes.php')
   ->direct(Request::uri(), Request::method());
