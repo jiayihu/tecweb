c78591cf9285f43fa2b50521b921565e3dc7f0f3
Elena Mattiazzo <ele.mattiazzo@gmail.com>
2018-02-07 19:04:15 +0100

    Aggiunti tag span lang

--- source/app/views/partials/public-header.partial.php
+++ source/app/views/partials/public-header.partial.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="icon" href="<?= ROOT ?>/favicon.ico?v=3" type="image/x-icon">
-  <title>Studio Sherlock & Watson</title>
+  <title lang="en">Studio Sherlock & Watson</title>
 
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   <link href="<?= ROOT ?>/public/css/external/normalize.css" rel="stylesheet">  
