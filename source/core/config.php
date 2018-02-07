a8b6efed827e68e6d7e7f55f66b0864df907f1ae
Jiayi Hu <jiayi.ghu@gmail.com>
2018-02-07 12:01:26 +0100

    Config: settati credenziali di Elena

--- source/core/config.php
+++ source/core/config.php
@@ -3,19 +3,19 @@
 namespace Core;
 
 // Differentiate from development and production, when the website is deployed in tecweb lab
-$production = true;
-$base_url = $production ? 'http://tecweb2016.studenti.math.unipd.it/ghu' : '';
+$production = false;
+$base_url = $production ? 'http://tecweb2016.studenti.math.unipd.it/emattiaz' : '';
 
-// Define a global constant to facilitate requiring CSS files
+// Define a global constant to facilitate requiring CSS files and images
 define('ROOT', $base_url);
 
 return [
   'production' => $production,
   'base_url' => $base_url,
   'database' => [
-    'name' => $production ? 'ghu' : 'sherlock',
-    'username'=> $production ? 'ghu' : 'root',
-    'password' => $production ? 'ahNgauMoong9aiVu' : 'password',
+    'name' => $production ? 'emattiaz' : 'sherlock',
+    'username'=> $production ? 'emattiaz' : 'root',
+    'password' => $production ? 'wae2ahngee3Eiphu' : 'password',
     'hostdb' => '127.0.0.1',
     'options' => [
       \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
