7de9c3d8537f596ca564bf3902a75e150f8fc6e5
Elena Mattiazzo <ele.mattiazzo@gmail.com>
2018-02-07 16:41:33 +0100

    Aggiunti tag abbr in home per acronimi

--- source/app/views/partials/public-footer.partial.php
+++ source/app/views/partials/public-footer.partial.php
@@ -1,6 +1,6 @@
     <footer class="footer">
       <div class="container clearfix">
-          <div class="left">&copy; 2018 <span lang="en">Sherlock and Watson</span> | Via Trieste, 63 - 35121 Padova (Italy) | PIVA 0123456789</div>
+          <div class="left">&copy; 2018 <span lang="en">Sherlock and Watson</span> | Via Trieste, 63 - 35121 Padova (Italy) | <abbr title="Partita dell'Imposizione Fiscale Indiretta">PIVA</abbr> 0123456789</div>
           <div class="right">
             <a class="uppercase" href="#top">Torna in cima</a>
           </div>
