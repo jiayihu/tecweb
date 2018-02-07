fb78edb9e24287a757c8399edf45876237e8d651
Jiayi Hu <jiayi.ghu@gmail.com>
2018-02-07 11:51:29 +0100

    Config: aggiunte credenziali del lab

--- source/core/config.php
+++ source/core/config.php
@@ -4,7 +4,7 @@ namespace Core;
 
 // Differentiate from development and production, when the website is deployed in tecweb lab
 $production = true;
-$base_url = $production ? 'http://tecweb1617.studenti.math.unipd.it/ghu' : '';
+$base_url = $production ? 'http://tecweb2016.studenti.math.unipd.it/ghu' : '';
 
 // Define a global constant to facilitate requiring CSS files
 define('ROOT', $base_url);
@@ -13,9 +13,9 @@ return [
   'production' => $production,
   'base_url' => $base_url,
   'database' => [
-    'name' => 'sherlock',
-    'username'=> 'root',
-    'password' => 'password',
+    'name' => $production ? 'ghu' : 'sherlock',
+    'username'=> $production ? 'ghu' : 'root',
+    'password' => $production ? 'ahNgauMoong9aiVu' : 'password',
     'hostdb' => '127.0.0.1',
     'options' => [
       \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
